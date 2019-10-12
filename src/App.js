134ecb61dce9b9a0700224967ef3cbe906967cb9
diego <diego_6525@hotmail.com>
2019-10-12 00:32:28 -0700

    modified todolist

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: ""
+    };
+  }
+
+  onInputChange = value => {
+    this.setState({ name: value });
+  };
+
+  render() {
+    return (
+      <div class="container">
+        <h1>Welcome to React</h1>
+        <input
+          placeholder="What is your name?"
+          value={this.state.name}
+          onChange={event => this.onInputChange(event.target.value)}
+        />
+        <p>Welcome {this.state.name}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
