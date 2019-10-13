a4c558797bff32d3684125cc4c938e64b8a17019
diego <diego_6525@hotmail.com>
2019-10-12 19:21:56 -0700

    created more components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import choreList from "./choreList";
+import navbar from "./navbar";
+import userUi from "./userUI";
+import Content from "./Content";
+
+export { choreList, navbar, userUi, Content };
