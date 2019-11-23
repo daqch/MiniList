59af7a8460538821520b0f313f20919de7f3f6d5
diego <diegoaquinochavez@uvic.ca>
2019-11-23 01:01:29 -0800

    app uses now hooks

--- src/App.js
+++ src/App.js
@@ -1,44 +1,55 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import * as components from "./components";
-class App extends Component {
-  constructor(props) {
-    super(props);
+import useSignUpForm from "./CustomHooks";
 
-    this.state = {
-      chores: [],
-      newChore: ""
-    };
+function App() {
+  const [chores, setChores] = useState([]);
+  const { inputs, handleInputChange, handleSubmit } = useSignUpForm();
+
+  function handleSubmit = e => {
+    continue;
   }
 
-  addChore = () => {
-    if (this.refs.chore.value.length > 4) {
-      this.setState(state => {
-        const chores = state.chores.concat(this.refs.chore.value);
-        console.log(chores);
-
-        return {
-          chores
-        };
-      });
-    }
-  };
-
-  displayState = () => {
-    console.log(this.state.chores);
-  };
-
-  render() {
-    return (
-      <div>
-        <components.navbar></components.navbar>
-        <div class="container">
-          <components.Content></components.Content>
-          <components.userUi action={this.addChore}></components.userUi>
+  return (
+    <div>
+      <components.navbar></components.navbar>
+      <div class="container">
+        <components.Content></components.Content>
+        <div class="row">
+          <div class="col">
+            <form class="form-inline" onSubmit={handle}>
+              <div class="form-group mb-2"></div>
+              <div class="form-group mx-sm-3 mb-2">
+                <label for="inputPassword2" class="sr-only">
+                  Password
+            </label>
+                <input
+                  onChange={handleInputChange}
+                  value={inputs.choreName}
+                  class="form-control"
+                  name="name"
+                  placeholder="Add a chore..."
+                ></input>
+              </div>
+              <button
+                type="submit"
+                class="btn btn-primary mb-2"
+              >
+                Add to list
+          </button>
+            </form>
+            <select name="importance" onChange={handleInputChange} class="form-control form-control-sm" style={{ width: "300px" }} placeholder="Importance">
+              <option>Important</option>
+              <option>Optional</option>
+              <option>Secondary</option>
+            </select>
+          </div>
+          <components.choreList chores={chores}></components.choreList>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
