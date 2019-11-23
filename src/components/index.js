0c56459db3d6a98ee7de8a0de5db34d182586cf5
diego <diegoaquinochavez@uvic.ca>
2019-11-23 00:55:26 -0800

    remobved userui.jsx

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import choreList from "./choreList";
 import navbar from "./navbar";
-import userUi from "./userUI";
 import Content from "./Content";
 
-export { choreList, navbar, userUi, Content };
+export { choreList, navbar, Content };
