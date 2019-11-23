6b46b34682e4bc5258814fd4de628ffbdcbdfdb2
diego <diego_6525@hotmail.com>
2019-11-23 05:15:40 -0800

    added hook for previewing

--- src/components/choreList.jsx
+++ src/components/choreList.jsx
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import Chore from "./Chore";
 
-function choreList(props) {
+function ChoreList(props) {
+  const [currentElement, changeElement] = useState({});
   const chores = props.chores;
-  const listItems = chores.map((chore) =>
-    <li className="list-group-item" key={chore}>{chore}</li>
-  );
+  const handleClick = e => {
+    const element = e.target;
+    const elements = e.target.parentElement.getElementsByTagName("li");
+    for (let item of elements) {
+      item.classList.remove("active");
+    }
+    element.classList.toggle("active");
+    var result = chores.filter(chore => {
+      return chore.name === element.innerHTML;
+    });
+    changeElement(result[0]);
+    console.log(currentElement);
+  };
+
+  const listItems = chores.map(chore => {
+    if (chore.importance === "Important")
+      return (
+        <li
+          className="list-group-item list-group-item-action list-group-item-warning"
+          key={chore.name}
+          onClick={handleClick}
+        >
+          {chore.name}
+        </li>
+      );
+    return (
+      <li
+        className="list-group-item list-group-item-action list-group-item-light"
+        key={chore.name}
+        onClick={handleClick}
+      >
+        {chore.name}
+      </li>
+    );
+  });
+
+  const chorePreview =
+    props.chores.length !== 0 ? (
+      <Chore chore={currentElement}></Chore>
+    ) : (
+      <p></p>
+    );
+
   return (
-    <div className="col">
-      <ul class="list-group">
-        {listItems}
-      </ul>
+    <div className="row">
+      <div class="col">
+        <ul className="list-group" style={{ width: "100%", marginTop: "30px" }}>
+          {listItems}
+        </ul>
+      </div>
+      <div className="col">{chorePreview}</div>
     </div>
   );
 }
 
-export default choreList;
+export default ChoreList;
