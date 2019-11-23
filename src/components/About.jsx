2c6a78db9870d5685fb3ea0d471dbb4922d4da18
diego <diego_6525@hotmail.com>
2019-11-23 05:16:48 -0800

    added about section

---
+++ src/components/About.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel"></h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            Add a Chore by tiping its name and its importance. Chores will be
+            aranged in importance order. Click over one to see more details
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
